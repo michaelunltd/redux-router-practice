07268e91e17071666ca3c8fb50196f01d43a69f8
Michael Rasheed Tan <mikkoyunltd@gmail.com>
2017-07-02 14:06:37 +0800

    implemented rendering posts

--- src/index.js
+++ src/index.js
@@ -1,15 +1,23 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import { BrowserRouter, Route } from 'react-router-dom';
+import promise from 'redux-promise';
 
-import App from './components/app';
+import PostsIndex from './components/posts_index';
+import PostsShow from './components/posts_show';
 import reducers from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
+    <BrowserRouter>
+      <div>
+        <Route path="/" component={PostsIndex} />
+        <Route path="/posts/:id"  component={PostsShow} />
+      </div> 
+    </BrowserRouter>
   </Provider>
   , document.querySelector('.container'));
