cf5f065f3bbd1c50850263313b2199b3b4b38f89
Michael Rasheed Tan <mikkoyunltd@gmail.com>
2017-08-03 23:47:31 +0800

    added routes for posts_show

--- src/index.js
+++ src/index.js
@@ -5,9 +5,11 @@ import { createStore, applyMiddleware } from 'redux';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import promise from 'redux-promise';
 
+import reducers from './reducers';
+
 import PostsIndex from './components/posts_index';
 import PostsNew from './components/posts_new';
-import reducers from './reducers';
+import PostsShow from './components/posts_show';
 
 const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
@@ -17,6 +19,7 @@ ReactDOM.render(
       <div>
         <Switch>
           <Route path="/posts/new" component={PostsNew} />
+          <Route path="/post/:id" component={PostsShow} />
           <Route path="/" component={PostsIndex} /> 
         </Switch>
       </div> 
