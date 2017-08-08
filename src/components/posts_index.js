af45a118ef5250eab0f70dca6381e0fba2e8caed
Michael Rasheed Tan <mikkoyunltd@gmail.com>
2017-08-08 22:20:59 +0800

    Refined delete post

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -22,7 +22,6 @@ class PostsIndex extends Component {
     }
 
     render() {
-        console.log(this.props.posts);
         return (
             <div>
                 <div className="text-xs-right">
